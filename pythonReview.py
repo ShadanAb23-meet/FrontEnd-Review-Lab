b2b15d59b5dbf6b41feccce243c92aa798f1d343
ShadanAb23-meet <109904715+ShadanAb23-meet@users.noreply.github.com>
2022-07-25 16:47:33 +0300

    Create pythonReview.py

---
+++ pythonReview.py
@@ -0,0 +1,13 @@
+def create_youtube_videos(title,description):
+	dict1={
+	"title" :title
+	"description" : description
+	"likes": 0
+	"dislikes": 0
+    }
+    comments={
+    "username": comment text
+    }
+
+def like():
+ 	pass 
