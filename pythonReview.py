a8f1551bf0c7a22cf781651805e53eebbb9b203b
ShadanAb23-meet <109904715+ShadanAb23-meet@users.noreply.github.com>
2022-07-25 18:57:40 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,13 +1,30 @@
 def create_youtube_videos(title,description):
-	dict1={
+	dict={
 	"title" :title
 	"description" : description
 	"likes": 0
 	"dislikes": 0
+    "comments": 0
     }
-    comments={
-    "username": comment text
-    }
+    return video
 
-def like():
- 	pass 
+def like(dict):
+	if like in dict:
+ 	dict ["likes"] = dict["likes"]+1
+ 	return dict 
+
+ def dislike(dict):
+ 	if dislikes in dict:
+ 	dict["disikes"]= dict["dislikes"]+1
+ 	return dict
+
+ 	
+ def add_comment(dict, username, comment_text):
+ 	dict["username"]["comments"]= comment_text
+ 	return dict
+
+youtubevideo = create_youtube_videos('hi','hi')
+for i in range(495):
+	like(create_youtube_videos)
+dislike(youtubevideo)
+add_comment(youtubevideo,"lk","lk")
